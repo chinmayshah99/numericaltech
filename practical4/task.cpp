b41f28eaf3cc864521d4375d95481016e8f1705d
Chinmay Shah <chinmayshah99@users.noreply.github.com>
2017-08-18 13:04:45 +0530

    Create task.cpp

---
+++ practical4/task.cpp
@@ -0,0 +1,32 @@
+// secant method using iteration
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#define f(x) (x*x*x)-6*(x*x)+11*(x)-7
+
+int main()
+{
+    int pl;
+    double a,b,c;
+    double fa,fb,fc;
+    cout<<"enter a,b"<<endl;
+    cin>>a>>b;
+    //cout<<"enter no iteration"<<endl;
+    //cout<<"enter precision";
+    //cin>>pl;
+    do
+    {
+        fa = f(a);
+        fb = f(b);
+        c =  b- (((b-a)*fb)/(fb - fa));
+        fc = f(c);
+        //cout<<i<<"\t"<<a<<" "<<fa<<" "<<b<<" "<<fb<<" "<<c<<" "<<fc<<endl;
+        a = b;
+        b = c;
+    }while(fabs(fc) > 10e-6);
+    cout<<endl;
+    cout.precision(8);
+    cout<<c;
+}
