3e419ecbf6fa342a359be40611164b4ef018cd52
chinmayshah99 <chinmayshah3899@gmail.com>
2017-11-13 10:02:33 +0530

    Updated

--- practical7/p.cpp
+++ practical7/p.cpp
@@ -43,9 +43,86 @@ int main()
         float pr = 1.0;
         for(int i = 0; i <=j; i++)
         {
-            pr=pr*((u-1)/(i+1));
+            pr=pr*((u-i)/(i+1));
         }
         y=y+(pr*diff[0][j]);
     }
     cout<<endl<<y;
 }
+
+
+/*
+
+//Forward interpolation
+ 
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main()
+{
+    double x,u,y,pr;
+    int size;
+    cout<<"How many values? \n";
+    cin>>size;
+    cout<<"Find y for what x?\n";
+    cin>>x;
+    int xarr[size], yarr[size], ydiff[size-1][size-1];
+    
+    for (int i=0; i<size; i++)
+    {
+        if(i==0)
+        {
+        cout<<"Enter x"<<i+1;
+        cout<<endl;
+        cin>>xarr[i];
+        cout<<"Enter y"<<i+1;
+        cout<<endl;
+        cin>>yarr[i];
+        }
+        
+        else
+        {
+        cout<<"Enter x"<<i+1;
+        cout<<endl;
+        cin>>xarr[i];
+        cout<<"Enter y"<<i+1;
+        cout<<endl;
+        cin>>yarr[i];
+        ydiff[i][0]=yarr[i]-yarr[i-1];
+        }
+    }
+    
+    for (int j=1; j<size-1; j++)
+    {
+        for(int i=0; i<size; i++)
+        {
+            ydiff[i][j]=(ydiff[i+1][j-1])-(ydiff[i][j-1]);
+        }
+    }
+    cout<<"x\ty\tdy\td2y\td3y \n";
+    for (int i=0; i<size; i++)
+    {
+        cout<<xarr[i]<<"\t"<<yarr[i]<<"\t"<<ydiff[i+1][0]<<"\t"<<ydiff[i+1][1]<<"\t"<<ydiff[i+1][2]<<"\n";
+    }
+    u=(x-xarr[0])/(xarr[1]-xarr[0]);
+    //y = yarr[0]+(u*ydiff[1][0])+(u*(u-1)/2*ydiff[1][1])+((u*(u-1)*(u-2)/6)*ydiff[1][2]);
+    
+    y=yarr[0];
+    for (int j=0; j<size-1; j++)
+    {
+        pr=1;
+        for (int i=0; i<=j; i++)
+        {
+            pr=pr*((u-i)/(i+1));
+            
+        }
+        cout<<pr<<endl;
+        cout<<y<<endl;
+        cout<<ydiff[1][j]<<endl;
+        y=y+pr*ydiff[1][j];
+    }
+    cout<<"y is - "<<y;
+}
+
+*/
